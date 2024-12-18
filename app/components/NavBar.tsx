d279b8e92d9f922c3ab267bcfe69267f216a05e9
Eli Bodovinitz <ebodovinitz@gmail.com>
2024-12-17 21:55:53 -0500

    updated resume link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -128,7 +128,7 @@ const NavBar = () => {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href={"EliBodovinitz_Resume.pdf"}
+          href={"EliBodovinitz.Resume.pdf"}
           className="relative group flex items-center py-1"
         >
           <span
